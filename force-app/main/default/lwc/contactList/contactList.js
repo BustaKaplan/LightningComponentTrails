a6d226c6d4798bd2931e9b98304d1aa5e34940fa
BustaKaplan <chelsea.kaplan3@gmail.com>
2020-11-24 10:00:21 -0600

    3/4 prework trails complete

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,22 @@
+import { LightningElement,wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
+import FNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+const COLUMNS = [
+    { label: 'Contact First Name', fieldName: FNAME_FIELD.fieldApiName, type: 'Text'},
+    { label: 'Contact Last Name', fieldName: LNAME_FIELD.fieldApiName, type: 'Text'},
+    { label: 'Contact Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'Text'}
+];
+
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+    get errors(){
+        return (this.contacts.error) ?
+        reduceErrors(this.contacts.error) : [];
+    }
+}
